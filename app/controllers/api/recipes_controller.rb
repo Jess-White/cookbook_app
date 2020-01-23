0155981c18e5917b0b57c311b23ea66f693ea16c
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-01-23 11:17:35 -0600

    added some search parameters in the controller

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,12 +1,47 @@
 class Api::RecipesController < ApplicationController
+  
+
   def index
-    @recipes = Recipe.all
+    search_term = params[:search]
+    @recipes = Recipe.where("ingredients iLIKE ? OR ingredients iLIKE ?", "%#{search_term }%", "%#{search_term }%")
     render 'index.json.jb'
+  end 
+
+  def create
+    @recipe = Recipe.new(
+                          title: params[:title],
+                          chef: params[:chef],
+                          ingredients: params[:ingredients],
+                          directions: params[:directions],
+                          prep_time: params[:prep_time]
+                          )
+    @recipe.save
+    render "show.json.jb"
   end 
 
   def show
     @recipe = Recipe.find(params[:id])
     render 'show.json.jb'
+  end 
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+
+    @recipe.save
+    render 'show.json.jb'
+  end 
+
+  def delete
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    @recipe.save
+    render json: {message: "Recipe #{@recipe.id} successfully destroyed"}
   end 
 end
 
